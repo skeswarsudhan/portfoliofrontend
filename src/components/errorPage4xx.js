825b90c38167d96ee8a9259d8d719f88116d9015
skeswarsudhan <skeswarsudhan@gmail.com>
2025-03-09 15:10:34 +0530

    errorpage commit

--- src/components/errorPage4xx.js
+++ src/components/errorPage4xx.js
@@ -5,7 +5,7 @@ const ErrorPage4xx = () => {
   return (
     <div className="error-container">
       <h1>404 - Page Not Found</h1>
-      <p>The page you are looking for does not exist or has been moved.</p>
+      <p>The page you are looking for does not exist.</p>
     </div>
   );
 };
